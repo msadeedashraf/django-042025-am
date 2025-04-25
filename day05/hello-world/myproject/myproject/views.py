095bf21dcacf1d92a017ec7b657a3801dda6f322
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-04-25 09:48:38 -0400

    .

--- day05/hello-world/myproject/myproject/views.py
+++ day05/hello-world/myproject/myproject/views.py
@@ -1,9 +1,12 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def homepage(request):
-    return HttpResponse("Hello World!")
+    # return HttpResponse("Hello World!")
+    return render(request, "myfirst.html")
 
 
 def contact(request):
-    return HttpResponse("Our Contact Page")
+    # return HttpResponse("Our Contact Page")
+    return render(request, "contact.html")
