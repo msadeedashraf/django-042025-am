3c89cdbafa1eeabccf48b979a5f5e866b42a4db4
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-05-12 10:16:25 -0400

    .

--- day16/jobzila/users/views.py
+++ day16/jobzila/users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 
 def register_view(request):
-    return render(request, "register.html")
+    form = UserCreationForm()
+    return render(request, "register.html", {"form": form})
