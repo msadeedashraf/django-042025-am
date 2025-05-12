056e9274b89f17dfbf59408d9d40e396ab4d5432
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-05-12 11:05:27 -0400

    .

--- day16/jobzila/users/views.py
+++ day16/jobzila/users/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
 
 
 def register_view(request):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            login(request, form.save())
+            return redirect("blogapp:blogpage")
+    else:
+        form = UserCreationForm()
+
     return render(request, "register.html", {"form": form})
